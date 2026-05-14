b8999eb9035772cde05661c91fc68e4b8020853b
Gea73 <geanccarlos2006@gmail.com>
2026-05-14 13:35:44 -0300

    feat:Firebase config

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCvprWH4kS_tNCoAtnUtiwhY5t33vUIT5M",
+    authDomain: "appconfeitariagean.firebaseapp.com",
+    projectId: "appconfeitariagean",
+    storageBucket: "appconfeitariagean.firebasestorage.app",
+    messagingSenderId: "175660946385",
+    appId: "1:175660946385:web:5c2ed2fa7fb04a640c86f0",
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export default db;
