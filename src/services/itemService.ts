702f7e653f527ab919107cf479363858bbf8b4e4
Gea73 <geanccarlos2006@gmail.com>
2026-05-15 16:42:06 -0300

    fix:item service now exported

--- src/services/itemService.ts
+++ src/services/itemService.ts
@@ -1,53 +1,56 @@
 import { Item } from "../models/item";
 import { itemRepo } from "../repositories/itemRepo";
 
-async function createItem(name: string, description: string, price: number, imageUrl: string) {
-    try {
-        const data = await itemRepo.createItem(name, description, price, imageUrl)
+export const itemService = {
+    createItem: async function (name: string, description: string, price: number, imageUrl: string) {
+        try {
+            const data = await itemRepo.createItem(name, description, price, imageUrl)
 
-        const item = new Item(name, description, price, imageUrl, data.createdAt, data.id)
-        return item;
+            const item = new Item(data.id,name, description, price, imageUrl, data.createdAt, )
+            return item;
 
-    } catch (error) {
-        throw new Error("Item can't be created", {
-            cause: error
-        })
-    }
-
-
-}
-
-async function updateItem(id: string, description: string | null, price: number | null, imageUrl: string | null) {
-    try {
-
-        if (description) {
-            await itemRepo.updateItemDescription(id, description)
-        }
-        if (price) {
-            await itemRepo.updateItemPrice(id, price)
-        }
-        if (imageUrl) {
-            await itemRepo.updateItemImage(id, imageUrl)
+        } catch (error) {
+            throw new Error("Item can't be created", {
+                cause: error
+            })
         }
 
-    } catch (error) {
-        throw new Error("Item can't be updated", {
-            cause: error
-        })
+
+    },
+
+    updateItem: async function (id: string, description: string | null, price: number | null, imageUrl: string | null) {
+        try {
+
+            if (description) {
+                await itemRepo.updateItemDescription(id, description)
+            }
+            if (price) {
+                await itemRepo.updateItemPrice(id, price)
+            }
+            if (imageUrl) {
+                await itemRepo.updateItemImage(id, imageUrl)
+            }
+
+        } catch (error) {
+            throw new Error("Item can't be updated", {
+                cause: error
+            })
+        }
+
+    },
+
+    getItem: async function (id: string | null, name: string | null) {
+        if (id) {
+            return await itemRepo.getItemById(id)
+        }
+        if (name) {
+            return await itemRepo.getItemByName(name)
+        }
+
+    },
+
+    deleteItem: async function (id: string) {
+        await itemRepo.deleteItem(id);
     }
 
-}
-
-async function getItem(id: string | null, name: string | null) {
-    if (id) {
-        return await itemRepo.getItemById(id)
-    }
-    if (name) {
-        return await itemRepo.getItemByName(name)
-    }
-
-}
-
-async function deleteItem(id: string) {
-    await itemRepo.deleteItem(id);
 }
