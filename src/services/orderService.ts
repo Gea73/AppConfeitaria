d8109d41f8b1e29baac9e0609f8a98be50e1f8d3
Gea73 <geanccarlos2006@gmail.com>
2026-06-08 22:16:05 -0300

    fix:Orderservice now returning correctly

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,9 +1,10 @@
 import { Order } from "@/models/order";
 import { orderRepo } from "@/repositories/orderRepo";
+import { OrderItem } from "@/types/ordemItem";
 
 export const orderService = {
 
-    createOrder: async function (customerId: string, items: object) {
+    createOrder: async function (customerId: string, items: OrderItem[]) {
         try {
 
             const data = await orderRepo.createOrder(customerId, items);
@@ -29,13 +30,28 @@ export const orderService = {
         }
 
     },
-    getOrder: async function (orderId: string | null, customerId: string | null) {
+    getOrder: async function (orderId: string | null): Promise<Order | null> {
+        let data = null;
         if (orderId) {
-            await orderRepo.getOrderById(orderId);
+            data = await orderRepo.getOrderById(orderId);
+            const order = new Order(orderId, data?.customerId, data?.items, data?.createdAt)
+            return order
         }
+
+        return null
+
+    },
+    getOrders: async function (customerId: string | null): Promise<Order[] | null> {
+
         if (customerId) {
-            await orderRepo.getOrdersByCustomer(customerId);
+           const data = await orderRepo.getOrdersByCustomer(customerId);
+            const orders = data?.map((o) => { return new Order(o.uid, o.customerId, o.items, o.createdAt) })
+            if (orders) {
+                return orders
+            }
+
         }
+        return null
     },
     deleteOrder: async function (orderId: string) {
         await orderRepo.deleteOrder(orderId)
