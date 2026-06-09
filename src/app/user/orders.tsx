6c7092a96f1ec8ef83363dd5e0135da653a349e1
Gea73 <geanccarlos2006@gmail.com>
2026-06-09 07:28:59 -0300

    fix:NoOrders now work as intended

--- src/app/user/orders.tsx
+++ src/app/user/orders.tsx
@@ -10,69 +10,67 @@ import { FlatList, StyleSheet } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Orders() {
+  const [user, setUser] = useState<User | null>(null);
+  const [orders, setOrders] = useState<Order[]>([]);
+  const [noOrders, setNoOrders] = useState<boolean>(false);
 
-    const [user, setUser] = useState<User | null>(null)
-    const [orders, setOrders] = useState<Order[]>([])
-    const [noOrders, setNoOrders] = useState<boolean>(false)
+  useEffect(() => {
+    async function getUser(): Promise<void> {
+      const uid = auth.currentUser?.uid;
+      if (uid) {
+        const user = await userService.getUser(uid, null);
+        setUser(user);
+      }
+    }
+    getUser();
+  }, []);
 
-    useEffect(() => {
-        async function getUser(): Promise<void> {
-            const uid = auth.currentUser?.uid
-            if (uid) {
-                const user = await userService.getUser(uid, null)
-                setUser(user)
-            }
+  useEffect(() => {
+    async function getOrders() {
+      const result = await orderService.getOrders(user?.getId() ?? null);
+      setOrders(result ?? []);
 
-        }
-        getUser()
+      if (orders.length <= 0) {
+        setNoOrders(true);
+        return;
+      }
+      setNoOrders(false);
+      return;
+    }
 
-    }, [])
+    if (user) {
+      getOrders();
+    }
+  }, [user]);
 
-
-
-    useEffect(() => {
-        async function getOrders() {
-            const result = await orderService.getOrders(user?.getId() ?? null)
-            setOrders(result ?? [])
-        }
-
-        if (user) {
-            getOrders()
-            if (orders.length === 0) {
-                setNoOrders(true)
-                return
-            }
-            setNoOrders(false)
-            return
-        }
-
-
-    }, [user])
-
-    return (<>
-        <SafeAreaProvider style={{ backgroundColor: "white" }}>
-            <SafeAreaView style={{ flex: 1 }}>
-                <NoOrders isVisible={noOrders}></NoOrders>
-                <FlatList
-                    data={orders}
-                    renderItem={({ item }) => (
-                        <OrderCard uid={item.getId()} items={item.getItems()} total={item.getItems().reduce((sum, i) => sum + i.price * i.quantity, 0)}
-
-                        />
-                    )}
-                    keyExtractor={(item) => item.getId()}>
-
-                </FlatList>
-
-            </SafeAreaView>
-        </SafeAreaProvider>
-    </>)
+  return (
+    <>
+      <SafeAreaProvider style={{ backgroundColor: "white" }}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <NoOrders isVisible={noOrders}></NoOrders>
+          <FlatList
+            data={orders}
+            renderItem={({ item }) => (
+              <OrderCard
+                uid={item.getId()}
+                items={item.getItems()}
+                total={item
+                  .getItems()
+                  .reduce((sum, i) => sum + i.price * i.quantity, 0)}
+              />
+            )}
+            keyExtractor={(item) => item.getId()}
+          ></FlatList>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </>
+  );
 }
 
 const stylesheet = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- src/components/noOrders.tsx
+++ src/components/noOrders.tsx
@@ -4,40 +4,41 @@ import { Button } from "./button";
 import { TopLogo } from "./topLogo";
 
 type NoOrdersProps = {
-    isVisible: boolean
-}
+  isVisible: boolean;
+};
 export default function NoOrders(props: NoOrdersProps) {
-    if (props.isVisible === true) {
-        return (
+  if (!props.isVisible) {
+    return null;
+  }
+  return (
+    <>
+      <TopLogo></TopLogo>
+      <View style={stylesheet.imageContainer}>
+        <Image
+          style={stylesheet.centerImage}
+          source={require("@/assets/images/noOrders.png")}
+        />
+      </View>
 
-            <>
-                <TopLogo></TopLogo>
-                <View style={stylesheet.imageContainer}>
-                    <Image style={stylesheet.centerImage} source={require("@/assets/images/noOrders.png")} />
-                </View>
-
-                <View style={stylesheet.container}>
-                    <Button text="Novo Pedido" onPress={() => router.push("/order/makeOrder")}></Button>
-
-                </View>
-            </>
-        )
-    }
-    else return;
+      <View style={stylesheet.container}>
+        <Button
+          text="Novo Pedido"
+          onPress={() => router.push("/order/makeOrder")}
+        ></Button>
+      </View>
+    </>
+  );
 }
 const stylesheet = StyleSheet.create({
-    imageContainer: {
-        alignItems: "center"
-    },
-    container: {
-        alignItems: "center"
-    },
-    centerImage: {
-        width: 350,
-        height: 300,
-        resizeMode: "contain"
-
-    },
-
-
-})
+  imageContainer: {
+    alignItems: "center",
+  },
+  container: {
+    alignItems: "center",
+  },
+  centerImage: {
+    width: 350,
+    height: 300,
+    resizeMode: "contain",
+  },
+});
