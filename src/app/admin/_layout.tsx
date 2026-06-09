4bd992790a05975dad5ec22d81db405adc74dda4
Gea73 <geanccarlos2006@gmail.com>
2026-06-09 07:28:29 -0300

    feat:Admin area started

---
+++ src/app/admin/_layout.tsx
@@ -0,0 +1,48 @@
+import { colors } from "@/styles/global";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          height: 120,
+          backgroundColor: colors.main,
+          borderTopColor: colors.secondary,
+        },
+        tabBarActiveTintColor: colors.mid,
+        tabBarInactiveTintColor: colors.secondary,
+      }}
+    >
+      <Tabs.Screen
+        name="orders"
+        options={{
+          title: "Orders",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="receipt-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Account",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ src/app/admin/account.tsx
@@ -0,0 +1,117 @@
+import { signOutUser } from "@/firebase/authentication";
+import { auth } from "@/firebase/firebaseConfig";
+import { User } from "@/models/user";
+import { userService } from "@/services/userService";
+import { colors, spacing, typography } from "@/styles/global";
+import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { useEffect, useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+export default function Account() {
+
+    const [user, setUser] = useState<User | null>(null)
+
+
+    useEffect(() => {
+        async function getUser(): Promise<void> {
+            const uid = auth.currentUser?.uid
+            if (uid) {
+                const user = await userService.getUser(uid, null)
+                setUser(user)
+            }
+
+        }
+        getUser()
+    }, [])
+
+
+
+
+
+
+
+
+
+    return (<>
+
+        <SafeAreaProvider style={{ backgroundColor: "white" }}>
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={stylesheet.header}>
+                    <Text style={stylesheet.headerText}>Patisserie</Text>
+                </View>
+
+                <View style={stylesheet.usernameContainer}>
+                    <Image source={require("@/assets/images/userImage.png")} />
+                    <Text style={stylesheet.usernameText}>{(user?.getName())}</Text>
+                </View>
+                <View style={stylesheet.cardContainer}>
+                    <Text numberOfLines={1} style={stylesheet.cardText}>Alterar dados</Text>
+                    <Ionicons name="information-circle-outline" size={30} color={colors.main} style={stylesheet.cardIcon}></Ionicons>
+                </View>
+                <View style={stylesheet.cardContainer}>
+                    <Text numberOfLines={1} style={stylesheet.cardText}>Endereços</Text>
+                    <Ionicons name="location" size={30} color={colors.main} style={stylesheet.cardIcon}></Ionicons>
+                </View>
+                <TouchableOpacity onPress={() => { signOutUser(); router.replace("/") }}>
+                    <View style={stylesheet.cardContainer}>
+                        <Text numberOfLines={1} style={stylesheet.cardText}>Sair</Text>
+                        <Ionicons name="exit-outline" size={30} color={colors.main} style={stylesheet.cardIcon}></Ionicons>
+                    </View>
+                </TouchableOpacity>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    </>)
+}
+
+const stylesheet = StyleSheet.create({
+    header: {
+        paddingVertical: 10,
+        backgroundColor: colors.main
+
+    },
+    headerText: {
+        color: colors.secondary,
+        fontSize: typography.title,
+        fontFamily: "Shafarik-Regular",
+        textAlign: "center"
+    },
+    usernameContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: colors.main,
+        width: "100%",
+        gap: "10%",
+        paddingHorizontal: "10%"
+    },
+    usernameText: {
+        color: colors.main,
+        fontSize: typography.subtitle,
+        fontWeight: "bold"
+    },
+    cardContainer: {
+        flexDirection: "row",
+        borderWidth: 0.5,
+        borderColor: colors.main,
+        width: "100%",
+        paddingTop: spacing.lg,
+        paddingBottom: spacing.lg,
+        alignItems: "center",
+        paddingLeft: "10%"
+
+    },
+    cardText: {
+        color: colors.main,
+        fontSize: typography.subtitle,
+        textAlign: "left",
+        fontWeight: "bold",
+        flex: 8
+
+    },
+    cardIcon: {
+        flex: 2,
+        position: "absolute",
+        left: "95%"
+    }
+})
