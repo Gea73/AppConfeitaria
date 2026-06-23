9ff0a398cad4e9bbeb7e83c8866abbe81afcb34b
Gea73 <geanccarlos2006@gmail.com>
2026-06-22 22:49:14 -0300

    fix:Better error handling

--- src/services/itemService.ts
+++ src/services/itemService.ts
@@ -6,8 +6,6 @@ import { itemRepo } from "../repositories/itemRepo";
 export const itemService = {
     createItem: async function (name: string, description: string, price: number, imageUrl: string) {
         try {
-
-
             const item = new Item(name, description, price, imageUrl);
             const data = await itemRepo.createItem(item);
             item.setUid(data.uid)
@@ -39,28 +37,49 @@ export const itemService = {
     },
 
     getItem: async function (uid: string) {
-        if (uid) {
+        try {
+
+
             const data = await itemRepo.getItemById(uid);
+            if (!data) {
+                throw new Error("Item can't be updated")
+            }
             const item = new Item(data?.name, data?.description, data?.price, data?.imageUrl, uid, data?.createdAt)
-            return item
+            return item;
 
+        } catch (error) {
+            throw new Error("Item can't be fetched", {
+                cause: error
+            })
         }
-
 
     },
 
     getItems: async function () {
-        const data = await itemRepo.getItems();
-        const items = data?.map((i) => { return new Item(i.name, i.description, i.price, i.imageUrl, i.uid, i.createdAt) })
-        if (items) {
+        try {
+            const data = await itemRepo.getItems();
+            if (!data) {
+                throw new Error("Item can't be fetched")
+            }
+            const items = data?.map((i) => { return new Item(i.name, i.description, i.price, i.imageUrl, i.uid, i.createdAt) })
             return items
-        }
-        return []
 
+        } catch (error) {
+            throw new Error("Item can't be fetched", {
+                cause: error
+            })
+        }
     },
 
     deleteItem: async function (uid: string) {
-        await itemRepo.deleteItem(uid);
+        try {
+            await itemRepo.deleteItem(uid);
+        } catch (error) {
+            throw new Error("Item can't be deleted", {
+                cause: error
+            })
+        }
+
     },
     subscribeToItems: function (callback: (items: Item[]) => void) {
         return onSnapshot(collection(db, "items"), (snapshot) => {
--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -2,15 +2,18 @@ import { db } from "@/firebase/firebaseConfig";
 import { Order } from "@/models/order";
 import { orderRepo } from "@/repositories/orderRepo";
 import { OrderItem } from "@/types/orderItem";
-import { orderStatus } from "@/types/orderStatus";
+import { OrderStatus } from "@/types/orderStatus";
 import { collection, onSnapshot, query, where } from "firebase/firestore";
 
 export const orderService = {
 
-    createOrder: async function (customerId: string, items: OrderItem[], status: orderStatus) {
+    createOrder: async function (customerId: string, items: OrderItem[], status: OrderStatus) {
         try {
             const order = new Order(customerId, items, status);
             const data = await orderRepo.createOrder(order);
+            if (!data) {
+                throw new Error("Order couldn't be created")
+            }
             order.setUid(data.uid)
             order.setCreatedAt(data.createdAt)
 
@@ -36,37 +39,72 @@ export const orderService = {
 
     },
     getOrder: async function (orderId: string | null): Promise<Order | null> {
-        let data = null;
-        if (orderId) {
-            data = await orderRepo.getOrderById(orderId);
-            const order = new Order(data?.customerId, data?.items, data?.status, orderId, data?.createdAt)
-            return order
-        }
-
-        return null
-
-    },
-    getOrders: async function (customerId: string | null): Promise<Order[] | null> {
-
-        if (customerId) {
-            const data = await orderRepo.getOrdersByCustomer(customerId);
-            const orders = data?.map((o) => { return new Order(o.customerId, o.items, o.status, o.uid, o.createdAt) })
-            if (orders) {
-                return orders
+        try {
+            if (orderId) {
+                const data = await orderRepo.getOrderById(orderId);
+                if (!data) {
+                    throw new Error("Order couldn't be fetched")
+                }
+                const order = new Order(data?.customerId, data?.items, data?.status, orderId, data?.createdAt)
+                return order
             }
 
+            return null
+        } catch (error) {
+            throw new Error("Order couldn't be fetched", {
+                cause: error
+            })
         }
-        return null
+    },
+    getOrders: async function (customerId: string | null): Promise<Order[] | null> {
+        try {
+            if (customerId) {
+                const data = await orderRepo.getOrdersByCustomer(customerId);
+                if (!data) {
+                    throw new Error("Order couldn't be fetched")
+                }
+
+                const orders = data?.map((o) => { return new Order(o.customerId, o.items, o.status, o.uid, o.createdAt) })
+                if (!orders) {
+                    throw new Error("Order couldn't be fetched")
+                }
+
+                return orders
+            }
+            return null
+        } catch (error) {
+            throw new Error("Order couldn't be fetched", {
+                cause: error
+            })
+        }
+
     },
     getAllOrders: async function () {
-        const data = await orderRepo.getOrders()
-        const orders = data?.map((o) => { return new Order(o.customerId, o.items, o.status, o.uid, o.createdAt) })
-        if (orders) {
+        try {
+            const data = await orderRepo.getOrders()
+            if (!data) {
+                throw new Error("Order couldn't be fetched")
+            }
+            const orders = data?.map((o) => { return new Order(o.customerId, o.items, o.status, o.uid, o.createdAt) })
+            if (!orders) {
+                throw new Error("Order couldn't be fetched")
+            }
             return orders
+        } catch (error) {
+            throw new Error("Order couldn't be fetched", {
+                cause: error
+            })
         }
+
     },
     deleteOrder: async function (orderId: string) {
-        await orderRepo.deleteOrder(orderId)
+        try {
+            await orderRepo.deleteOrder(orderId)
+        } catch (error) {
+            throw new Error("Order couldn't be deleted", {
+                cause: error
+            })
+        }
     },
     subscribeToAllOrders: function (callback: (orders: Order[]) => void) {
         return onSnapshot(collection(db, "orders"), (snapshot) => {
@@ -78,7 +116,7 @@ export const orderService = {
         })
 
     },
-    subscribeToOrders: function (customerId: string, callback: (orders: Order[]) => void) {
+    subscribeToCustomerOrders: function (customerId: string, callback: (orders: Order[]) => void) {
 
         const q = query(collection(db, "orders"), where("customerId", "==", customerId));
 
