97811927cba26c3df91c2ea0b4b43675fc3245bd
Gea73 <geanccarlos2006@gmail.com>
2026-06-26 16:11:40 -0300

    fix:loadingwheel while the layout is mounting

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,19 @@
+import LoadingWheel from "@/components/loadingWheel";
 import { auth } from "@/firebase/firebaseConfig";
 import { userService } from "@/services/userService";
 import { router, Stack } from "expo-router";
 import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Layout() {
+  const [loading, setLoading] = useState<boolean>(true);
+
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (userSignIn) => {
       try {
         if (userSignIn) {
           const user = await userService.getUser(userSignIn.uid);
+
           router.replace(
             user?.getRole() === "admin" ? "/admin/home" : "/user/home",
           );
@@ -19,10 +23,17 @@ export default function Layout() {
         router.replace("/auth/signIn");
       } catch (error) {
         router.replace("/auth/signIn");
+      } finally {
+        setLoading(false);
       }
     });
+
     return () => unsubscribe();
   }, []);
+
+  if (loading) {
+    return <LoadingWheel></LoadingWheel>;
+  }
 
   return <Stack screenOptions={{ headerShown: false }} />;
 }
