717fa48d006806f40665c912c54575c8af1ccfc4
Gea73 <geanccarlos2006@gmail.com>
2026-06-29 13:32:53 -0300

    feat:DB connection working

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { v7 as uuidv7 } from "uuid";
+import { userRepo } from "../repositories/userRepo.js";
 
-const app = express()
+const app = express();
 app.use(express.json({ limit: "10kb" }));
-
-
+const id = uuidv7();
+userRepo.createUser(id, "teste", "senhateste", "email@email", "admin");
 export { app };
+
