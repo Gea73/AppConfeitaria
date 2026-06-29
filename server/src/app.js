3c9b040f3b1ec74a3bd3df5f107cb4a559734ea7
Gea73 <geanccarlos2006@gmail.com>
2026-06-29 13:12:13 -0300

    Revert "feat:db connection working"
    This reverts commit aa38bf644b7b845570ecdfed9d570338583aee8f.

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,7 @@
 import express from "express";
-import { userRepo } from "../repositories/userRepo.js";
-import { v7 as uuidv7 } from "uuid";
 
-const app = express();
+const app = express()
 app.use(express.json({ limit: "10kb" }));
-const id = uuidv7();
-userRepo.createUser(id, "teste", "senhateste", "email@email", "admin");
+
+
 export { app };
