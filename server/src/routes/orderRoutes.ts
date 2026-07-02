9d35fc4e0ace7c7d7e81b2314035a8fc1c477f5f
Gea73 <geanccarlos2006@gmail.com>
2026-07-02 11:07:39 -0300

    fix:routes order fixed

--- server/src/routes/orderRoutes.ts
+++ server/src/routes/orderRoutes.ts
@@ -4,11 +4,11 @@ import { controller } from "../controllers/orderController.js";
 
 router.post('/', (req, res) => controller.createOrder(req, res))
 
-router.get('/:id', (req, res) => controller.getOrder(req, res))
+router.get('/all', (req, res) => controller.getAllOrders(req, res))
 
 router.get('customer/:customerId', (req, res) => controller.getOrders(req, res))
 
-router.get('/all', (req, res) => controller.getAllOrders(req, res))
+router.get('/:id', (req, res) => controller.getOrder(req, res))
 
 router.put('/:id', (req, res) => controller.updateOrder(req, res))
 
