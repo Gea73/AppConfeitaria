92c754612a36209afc8cdd23c695d6788075b439
Gea73 <geanccarlos2006@gmail.com>
2026-07-03 16:05:26 -0300

    fix:routes fixing

--- server/src/routes/orderRoutes.ts
+++ server/src/routes/orderRoutes.ts
@@ -1,12 +1,12 @@
 import express from "express";
-const router = express.Router()
 import { controller } from "../controllers/orderController.js";
+const router = express.Router()
 
 router.post('/', (req, res) => controller.createOrder(req, res))
 
 router.get('/all', (req, res) => controller.getAllOrders(req, res))
 
-router.get('customer/:customerId', (req, res) => controller.getOrders(req, res))
+router.get('/customer/:customerId', (req, res) => controller.getOrders(req, res))
 
 router.get('/:id', (req, res) => controller.getOrder(req, res))
 
@@ -14,4 +14,4 @@ router.put('/:id', (req, res) => controller.updateOrder(req, res))
 
 router.delete('/:id', (req, res) => controller.deleteOrder(req, res))
 
-export { router } 
+export { router };
